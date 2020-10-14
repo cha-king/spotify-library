fb13c2fd7593e709e644852907ef6c951e309259
cha-king <charlie.king4967@gmail.com>
2020-10-13 20:30:58 -0700

    Fix typo with scope auth param

--- auth.js
+++ auth.js
@@ -1,13 +1,13 @@
 const API_URL = 'https://accounts.spotify.com/authorize';
 const CLIENT_ID = 'ebded317aa0c41048b1cd4ac05c6c37d';
-const REDIRECT_URL = 'https://library.cha-king.com'
+const REDIRECT_URL = 'http://localhost';
 
 document.getElementById('auth-button').onclick = async () => {
     let url = API_URL;
     url += '?client_id=' + encodeURIComponent(CLIENT_ID);
     url += '&response_type=token' 
     url += '&redirect_uri=' + encodeURIComponent(REDIRECT_URL);
-    url += '&scope' + 'user-library-read';
+    url += '&scope=' + 'user-library-read';
     /*
     const response = await fetch(url);
     const response_data = await response.json();
