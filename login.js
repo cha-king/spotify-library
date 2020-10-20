e66609afd7426fc88472ae3aea30fcbfbe144ce2
cha-king <charlie.king4967@gmail.com>
2020-10-20 00:33:53 -0700

    Add query params to redirect URIs

--- login.js
+++ login.js
@@ -1,12 +1,12 @@
 const API_URL = 'https://accounts.spotify.com/authorize';
 const CLIENT_ID = 'ebded317aa0c41048b1cd4ac05c6c37d';
-const REDIRECT_URL = 'https://library.cha-king.com';
+const REDIRECT_URL = 'https://library.cha-king.com?display=artist';
 
 document.getElementById('auth-button').onclick = async () => {
     const state = Math.random().toString(36).substring(2);
     window.sessionStorage.setItem('state', state);
 
-    const redirect_uri = (window.location.hostname === 'localhost') ? 'http://localhost' : REDIRECT_URL;
+    const redirect_uri = (window.location.hostname === 'localhost') ? 'http://localhost/?display=artist' : REDIRECT_URL;
 
     let url = API_URL;
     url += '?client_id=' + encodeURIComponent(CLIENT_ID);
