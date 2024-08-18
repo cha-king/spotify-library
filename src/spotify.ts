d82dbcc601e9add3b600c57f00e145f731ad832f
Charlie King <charlie.king4967@gmail.com>
2024-08-18 01:25:41 -0700

    Fetch whole library concurrently

--- src/spotify.ts
+++ src/spotify.ts
@@ -1,8 +1,11 @@
 import { SPOTIFY_API_URL } from "./constants";
 import { Token } from "./types";
 
+const LIMIT = 50;
+
 interface AlbumsResponse {
   items: Item[];
+  total: number;
 }
 
 interface Item {
@@ -11,19 +14,47 @@ interface Item {
 
 interface Album {}
 
-export async function getAlbums({ access_token }: Token): Promise<Album[]> {
+async function fetchAlbums(access_token: string, offset: number) {
   const url = new URL(SPOTIFY_API_URL);
   url.search = new URLSearchParams({
-    limit: "50",
+    limit: LIMIT.toString(),
+    offset: offset.toString(),
   }).toString();
 
-  const response = (await fetch(url, {
+  const { items } = (await fetch(url, {
     headers: {
       Authorization: `Bearer ${access_token}`,
     },
   }).then((response) => response.json())) as AlbumsResponse;
 
-  const albums = response.items.map((item) => item.album);
+  const albums = items.map((item) => item.album);
+
+  return albums;
+}
+
+export async function getAlbums({ access_token }: Token): Promise<Album[]> {
+  const url = new URL(SPOTIFY_API_URL);
+  url.search = new URLSearchParams({
+    limit: "1",
+    offset: "0",
+  }).toString();
+
+  const { total } = (await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+    },
+  }).then((response) => response.json())) as AlbumsResponse;
+
+  const promises = [];
+  for (let offset = 0; offset < total; offset += LIMIT) {
+    const promise = fetchAlbums(access_token, offset);
+    promises.push(promise);
+  }
+
+  const albums = (await Promise.all(promises)).reduce(
+    (albums, albumList) => [...albums, ...albumList],
+    []
+  );
 
   return albums;
 }
