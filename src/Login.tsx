357219680cd3a6439314b8692a3d672a285daae6
Charlie King <charlie.king4967@gmail.com>
2024-08-17 21:43:17 -0700

    Add initial Login component

---
+++ src/Login.tsx
@@ -0,0 +1,11 @@
+export default function Login() {
+  return (
+    <div>
+      <h2>Log In</h2>
+      <p>Spotify access is required to continue</p>
+      <button>
+        <span>Log in with Spotify</span>
+      </button>
+    </div>
+  );
+}
